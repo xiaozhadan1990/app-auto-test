561147bb7958f44bf3f8a3ef1d787c274560df7f
牛牛最牛 <xujianbin519@163.com>
2026-03-27 21:22:26 +0800

    增加在用例执行页面中reyee的版本统计

--- web-ui/src/components/DeviceSummaryCards.tsx
+++ web-ui/src/components/DeviceSummaryCards.tsx
@@ -18,17 +18,17 @@ function DeviceSummaryCards({
 }: DeviceSummaryCardsProps) {
   return (
     <Row gutter={10} style={{ marginTop: 12 }}>
-      <Col span={5}>
+      <Col span={4}>
         <Card size="small" title="手机品牌" style={summaryCardStyle} styles={{ body: summaryBodyStyle }}>
           {renderBrand(currentDevice?.brand, 56)}
         </Card>
       </Col>
-      <Col span={5}>
+      <Col span={4}>
         <Card size="small" title="设备型号" style={summaryCardStyle} styles={{ body: summaryBodyStyle }}>
           <span style={summaryValueStyle}>{currentDevice?.model || "-"}</span>
         </Card>
       </Col>
-      <Col span={5}>
+      <Col span={4}>
         <Card size="small" title="系统版本" style={summaryCardStyle} styles={{ body: summaryBodyStyle }}>
           <span style={summaryValueStyle}>{currentDevice?.os_version || "-"}</span>
         </Card>
@@ -40,10 +40,17 @@ function DeviceSummaryCards({
           </span>
         </Card>
       </Col>
-      <Col span={5}>
+      <Col span={4}>
         <Card size="small" title="ruijieCloud 版本" style={summaryCardStyle} styles={{ body: summaryBodyStyle }}>
           <span style={summaryValueStyle}>
             {(currentDevice?.app_versions && currentDevice.app_versions.ruijieCloud) || "-"}
+          </span>
+        </Card>
+      </Col>
+      <Col span={4}>
+        <Card size="small" title="Reyee 版本" style={summaryCardStyle} styles={{ body: summaryBodyStyle }}>
+          <span style={summaryValueStyle}>
+            {(currentDevice?.app_versions && currentDevice.app_versions.reyee) || "-"}
           </span>
         </Card>
       </Col>
